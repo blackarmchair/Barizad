c8e26c4535a93023bf3c456bd599eb558325180d
shahsity <taj@sity.ai>
2025-06-12 10:54:09 -0500

    Fix path

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "Barizad",
+    baseUrl: "/Barizad",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
